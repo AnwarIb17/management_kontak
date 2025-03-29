5b5a295786ccf49b48f868e86bfeaa77413e86ac
lordh <ibrahimeri127@gmail.com>
2025-03-29 13:43:36 +0700

    menggunakan file dan except handling/error

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+# program management kontak
+
+def membuka_kontak(path='kontak.txt'):
+    with open(path, mode='r') as file:
+        kontak = file.readlines()
+    return kontak
+
+
+def menyimpan_kontak(path='kontak.txt', isi=None):
+    if isi is None:
+        isi = []
+    with open(path, mode='w') as file:
+        file.writelines(isi)
+
+
 class Kontak:
     def __init__(self):
-        self.kontak = []
+        self.kontak = membuka_kontak()
 
     def melihat_kontak(self):
         if self.kontak:
             for num, item in enumerate(self.kontak, start=1):
-                print(f'{num}, {item["nama"]}, {item["hp"]}, {item["email"]}')
+                print(f'{num}, ' + item)
         else:
             print("\nkontak masih kosong !! \n")
             return False
@@ -15,7 +30,7 @@ class Kontak:
         nama = input("masukkan nama kontak baru : ")
         hp = input("masukkan nomor kontak baru : ")
         email = input("masukkan email kontak baru : ")
-        kontak_baru = {'nama': nama, 'hp': hp, 'email': email}
+        kontak_baru = f'{nama}, {hp}, {email}' + '\n'
         self.kontak.append(kontak_baru)
         print("\nkontak baru berhasil di tambahkan ! \n")
 
@@ -23,9 +38,15 @@ class Kontak:
         if not self.melihat_kontak():
             return
         else:
-            i_hapus = int(input("\nmasukkan nomor kontak yang ingin di hapus : ")) - 1
-            del self.kontak[i_hapus]
-            print("kontak berhasil di hapus !")
+            try:
+                i_hapus = int(input("\nmasukkan nomor kontak yang ingin di hapus : ")) - 1
+                del self.kontak[i_hapus]
+                print("kontak berhasil di hapus !")
+            except:
+                print("pilihan anda tidak tersedia")
+
+    def keluar_kontak(self):
+        menyimpan_kontak(isi=self.kontak)
 
 
 kontak_kantor = Kontak()
@@ -39,7 +60,7 @@ while True:
     print("3. Menghapus kontak")
     print("4. keluar")
     print("-" * 35)
-    pilihan = input("Masukkan pilihan anda (1,2,3) : ")
+    pilihan = input("Masukkan pilihan anda (1,2,3,4) : ")
 
     if pilihan == '1':
         kontak_kantor.melihat_kontak()
@@ -51,6 +72,7 @@ while True:
         kontak_kantor.menghapus_kontak()
 
     elif pilihan == '4':
+        kontak_kantor.keluar_kontak()
         break
     else:
         print("\npilihan yang anda masukkan tidak valid!!!\n")
